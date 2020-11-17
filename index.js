f528bff36759c28a18c59c930046c9a3651689d6
Jehyun <congmul@hotmail.com>
2020-11-17 00:00:18 -0800

    Add menu inquirer

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const Title = require("./assets/js/appTitle");
-const inquierer = require("inquirer");
+const inquirer = require("inquirer");
 
 let title = new Title();
 
 title.titleLogo();
+
+menuInquirer();
+
+function menuInquirer(){
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message : "What woud you like to do?",
+                name: "menu",
+                choices: [
+                    "View All Employees",
+                    "View All Employees By Department",
+                    "View All Employees By Manager",
+                    "Add Employees",
+                    "Remove Employee",
+                    "Update Employee Role",
+                    "Update Employee Manager"
+                ]
+            }
+        ]).then(res => {
+            console.log(res);
+        })
+}
