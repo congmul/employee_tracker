6da53c2aff50d34329389f3b4a368a5a9acff022
Jehyun <congmul@hotmail.com>
2020-11-18 22:15:00 -0800

    Complete ADD inquirer

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n\n");
 
     title.titleLogo();
@@ -29,13 +29,13 @@ connection.connect((err) => {
 
 
 
-function menuInquirer(){
+function menuInquirer() {
     let order = "";
     inquirer
         .prompt([
             {
                 type: "rawlist",
-                message : "What woud you like to do?",
+                message: "What woud you like to do?",
                 name: "menu",
                 choices: [
                     "View All Employees",
@@ -50,47 +50,47 @@ function menuInquirer(){
             }
         ]).then(res => {
             // console.log(res);
-            if(res.menu === "EXIT"){
+            if (res.menu === "EXIT") {
                 console.log("Close App...");
                 connection.end();
                 return;
-            }else if(res.menu === "View All Employees"){
+            } else if (res.menu === "View All Employees") {
                 order = "ORDER BY id"
                 viewSql(order);
-            }else if(res.menu === "View All Employees By Department"){
+            } else if (res.menu === "View All Employees By Department") {
                 order = "ORDER BY department"
                 viewSql(order);
-            }else if(res.menu === "View All Employees By Manager"){
+            } else if (res.menu === "View All Employees By Manager") {
                 order = "ORDER BY manager"
                 viewSql(order);
-            }else if(res.menu === "Add Employees"){
+            } else if (res.menu === "Add Employees") {
                 order = "ORDER BY manager"
                 addSql();
-            }else if(res.menu === "Remove Employee"){
+            } else if (res.menu === "Remove Employee") {
                 deleteSql();
             }
-            
+
         })
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // SQL
-function viewSql(order){
+function viewSql(order) {
     connection.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name,' ',m.last_name) AS manager
     FROM employee e
     JOIN role r ON e.role_id = r.id
     JOIN department d ON r.department_id = d.id
     LEFT JOIN employee m ON e.manager_id = m.id
     ${order};`,
-        function(err, res) {
-            if(err) throw err;
+        function (err, res) {
+            if (err) throw err;
             console.log();
             console.log(`id  first_name  last_name   title               department    salary      manager`);
             console.log(`--  ----------  ----------  ------------------  ------------  ----------  -------------------`);
-            for(obj of res){
-                
+            for (obj of res) {
+
                 console.log(String(obj.id).padEnd(4) + obj.first_name.padEnd(12) + obj.last_name.padEnd(12) + obj.title.padEnd(20) + obj.department.padEnd(14) + String(obj.salary).padEnd(12) + obj.manager);
-                
+
             }
             console.log();
             menuInquirer();
@@ -98,67 +98,59 @@ function viewSql(order){
     );
 }
 
-function addSql(){
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message : "What is the employee's first name?",
-                name: "first_name"
-            },
-            {
-                type: "input",
-                message : "What is the employee's last name?",
-                name: "last_name"
-            },
-            {
-                type: "rawlist",
-                message : "What is the employee's role?",
-                name: "role",
-                choices: ["sales Lead", "salesperson"]
-            },
-            {
-                type: "rawlist",
-                message : "Who is the employee's manager?",
-                name: "managerName",
-                choices: ["Jehyun Jung", "Nayeon Kang"]
+function addSql() {
+    connection.query(`SELECT CONCAT(first_name,' ',last_name) AS name, r.title 
+    FROM employee e JOIN role r ON e.role_id = r.id;`,
+        function (err, res) {
+            if (err) throw err;
+            let nameArr = [];
+            let roleArr = [];
+            nameArr.push("None");
+            for (obj of res) {
+                nameArr.push(obj.name);
+                roleArr.push(obj.title);
             }
-        ]).then((res) => {
-            console.log(res);
-            console.log(`Added ${res.first_name} ${res.last_name} to the database`);
-            console.log();
-            menuInquirer();
-        })
-
-    // connection.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name,' ',m.last_name) AS manager
-    // FROM employee e
-    // JOIN role r ON e.role_id = r.id
-    // JOIN department d ON r.department_id = d.id
-    // LEFT JOIN employee m ON e.manager_id = m.id
-    // ${order};`,
-    //     function(err, res) {
-    //         if(err) throw err;
-    //         console.log();
-    //         console.log(`id  first_name  last_name   title               department    salary      manager`);
-    //         console.log(`--  ----------  ----------  ------------------  ------------  ----------  -------------------`);
-    //         for(obj of res){
-                
-    //             console.log(String(obj.id).padEnd(4) + obj.first_name.padEnd(12) + obj.last_name.padEnd(12) + obj.title.padEnd(20) + obj.department.padEnd(14) + String(obj.salary).padEnd(12) + obj.manager);
-                
-    //         }
-    //         console.log();
-    //         menuInquirer();
-    //     }
-    // );
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the employee's first name?",
+                        name: "first_name"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the employee's last name?",
+                        name: "last_name"
+                    },
+                    {
+                        type: "rawlist",
+                        message: "What is the employee's role?",
+                        name: "role",
+                        choices: roleArr
+                    },
+                    {
+                        type: "rawlist",
+                        message: "Who is the employee's manager?",
+                        name: "managerName",
+                        choices: nameArr
+                    }
+                ]).then((res) => {
+                    console.log(res);
+                    console.log(`Added ${res.first_name} ${res.last_name} to the database`);
+                    console.log();
+                    menuInquirer();
+                })
+        }
+    )
 }
 
-function deleteSql(){
+function deleteSql() {
     connection.query(`SELECT CONCAT(first_name,' ',last_name) AS name
     FROM employee;`,
-        function(err, res) {
-            if(err) throw err;
+        function (err, res) {
+            if (err) throw err;
             let nameArr = [];
-            for(obj of res){
+            for (obj of res) {
                 nameArr.push(obj.name);
             }
             nameArr.push("CANCEL");
@@ -166,14 +158,14 @@ function deleteSql(){
                 .prompt([
                     {
                         type: "rawlist",
-                        message : "Which employee do you want to remove?",
+                        message: "Which employee do you want to remove?",
                         name: "name",
                         choices: nameArr
                     }
                 ]).then((res) => {
-                    if(res.name === "CANCEL"){
+                    if (res.name === "CANCEL") {
                         menuInquirer();
-                    }else{
+                    } else {
                         connection.query("DELETE FROM employee WHERE CONCAT(first_name,' ',last_name) = ?", [res.name]);
                         console.log("Removed employee from the database");
                         console.log();
